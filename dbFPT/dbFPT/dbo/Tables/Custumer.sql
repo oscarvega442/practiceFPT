cba3022fe7af49e28dad8bcb8a74b42cd74383b2
oscarvega442 <77636851+oscarvega442@users.noreply.github.com>
2021-12-22 10:56:08 -0600

    Add files via upload

---
+++ dbFPT/dbFPT/dbo/Tables/Custumer.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Custumer] (
+    [idCustomer] INT        IDENTITY (1, 1) NOT NULL,
+    [name]       NCHAR (10) NULL,
+    [phone]      INT        NULL,
+    [email]      NCHAR (10) NULL,
+    [notes]      NCHAR (10) NULL,
+    CONSTRAINT [PK_Custumer] PRIMARY KEY CLUSTERED ([idCustomer] ASC)
+);
+
